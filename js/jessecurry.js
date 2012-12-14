b70ad07cc5c243ce9bf5fb11e25ae82d4c9958fe
jessecurry <jesse@jessecurry.net>
2012-12-13 19:32:21 -0500

    Retool

--- js/jessecurry.js
+++ js/jessecurry.js
@@ -1,11 +1,7 @@
 $(document).ready(function() {
-  var $document = $(document);
   var $body = $('body');
-  var setBodyScale = function() {
-      
+  var setBodyScale = function() {  
       var scale = $body.width() / 960.0;
-      if ( $body.width() > $document.height() )
-        scale = $document.height() / 960.0;
       
       var fontSize = 16.0 * scale;
       $('body').css('font-size', fontSize + 'px');
